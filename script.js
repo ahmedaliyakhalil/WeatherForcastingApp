210faa20c1d0726feab36a40d81a54ebc51966a8
ahmedaliyakhalil <142332632+ahmedaliyakhalil@users.noreply.github.com>
2023-10-30 22:37:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7c3fb41f99msh204aee4b0502816p1ce7d1jsn44a3a6bcea8d',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+const getWeather = (city)=> {
+
+        cityName.innerHTML=city
+        fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city,options)
+        .then(response => response.json()) 
+        .then((response) => {
+        
+        console.log(response)
+        
+        // cloud_pct.innerHTML = response.cloud_pct
+        temp.innerHTML = response.temp
+        feels_like.innerHTML = response.feels_like
+        humidity.innerHTML = response.humidity
+        min_temp.innerHTML = response.min_temp
+        max_temp.innerHTML = response.max_temp
+        wind_speed.innerHTML = response.wind_speed
+        wind_degrees.innerHTML = response.wind_degrees
+        sunrise.innerHTML = response.sunrise
+        sunset.innerHTML = response.sunset
+    })
+    .catch(err => console.error(err));  
+}
+
+submit.addEventListener("click",() => {
+    e.preventDefault()
+    getWeather(city.value)
+
+})
+
+getWeather("Delhi")
